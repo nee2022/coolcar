c07481bb6cda1f89598bd7e618f5ad0c368b2fc7
ccmouse <ccmouse@gmail.com>
2020-09-08 14:56:02 +0800

    use promise to handle avatar/nickname loads

--- wx/miniprogram/utils/util.ts
+++ wx/miniprogram/utils/util.ts
@@ -1,14 +1,9 @@
 export const formatTime = (date: Date) => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
   const hour = date.getHours()
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
   return (
-    [year, month, day].map(formatNumber).join('/') +
-    ' ' +
     [hour, minute, second].map(formatNumber).join(':')
   )
 }
@@ -16,4 +11,22 @@ export const formatTime = (date: Date) => {
 const formatNumber = (n: number) => {
   const s = n.toString()
   return s[1] ? s : '0' + s
+}
+
+export function getSetting(): Promise<WechatMiniprogram.GetSettingSuccessCallbackResult> {
+  return new Promise((resolve, reject) => {
+    wx.getSetting({
+      success: resolve,
+      fail: reject,
+    })
+  })
+}
+
+export function getUserInfo(): Promise<WechatMiniprogram.GetUserInfoSuccessCallbackResult> {
+  return new Promise((resolve, reject) => {
+    wx.getUserInfo({
+      success: resolve,
+      fail: reject,
+    })
+  })
 }
