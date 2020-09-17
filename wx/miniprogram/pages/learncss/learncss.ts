8126780f6072a2a820defab8847b5d25666239d2
ccmouse <ccmouse@gmail.com>
2020-09-17 20:14:01 +0800

    add learncss page

---
+++ wx/miniprogram/pages/learncss/learncss.ts
@@ -0,0 +1,19 @@
+Page({
+  data: {
+    showPath: true,
+    values: [
+      {
+        name: 'john',
+        id: 1,
+      },
+      {
+        name: 'mary',
+        id: 2,
+      },
+      {
+        name: 'tom',
+        id:3,
+      },
+    ],
+  },
+})
