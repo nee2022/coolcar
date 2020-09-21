0f827d647a17eff75a0c32a588e8064c969717a2
ccmouse <ccmouse@gmail.com>
2020-09-21 13:31:34 +0800

    index page map

--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -1,52 +1,62 @@
-// index.ts
-// 获取应用实例
-const app = getApp<IAppOption>()
-
 Page({
   data: {
-    motto: 'Hello World from typescript',
-    userInfo: {},
-    hasUserInfo: false,
+    setting: {
+      skew: 0,
+      rotate: 0,
+      showLocation: true,
+      showScale: true,
+      subKey: '',
+      layerStyle: -1,
+      enableZoom: true,
+      enableScroll: true,
+      enableRotate: false,
+      showCompass: false,
+      enable3D: false,
+      enableOverlooking: false,
+      enableSatellite: false,
+      enableTraffic: false,
+    },
+    location: {
+      latitude: 31,
+      longitude: 120,
+    },
+    scale: 10,
+    markers: [
+      {
+        iconPath: "/resources/car.png",
+        id: 0,
+        latitude: 23.099994,
+        longitude: 113.324520,
+        width: 50,
+        height: 50
+      },
+      {
+        iconPath: "/resources/car.png",
+        id: 1,
+        latitude: 23.099994,
+        longitude: 114.324520,
+        width: 50,
+        height: 50
+      },
+    ]
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.redirectTo({
-      url: '../logs/logs',
-    })
-  },
-  async onLoad() {
-    const userInfo = await app.globalData.userInfo
-    this.setData({
-      userInfo,
-      hasUserInfo: true,
-    })
-
-    // this.updateMotto()
-  },
-  getUserInfo(e: any) {
-    console.log(e)
-    const userInfo: WechatMiniprogram.UserInfo = e.detail.userInfo
-    app.resolveUserInfo(userInfo)
-  },
-  updateMotto() {
-    // 在10秒之内持续更新Motto
-    let shouldStop = false
-    setTimeout(() => {
-      shouldStop = true
-    }, 10000);
-
-    let count = 0
-    const update = () => {
-      count++
-      if (!shouldStop) {
+  onMyLocationTap() {
+    wx.getLocation({
+      type: 'gcj02',
+      success: res => {
         this.setData({
-          motto: `Update count: ${count}`,
-        }, () => {
-          update()
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude,
+          },
+        })
+      }, 
+      fail: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '请前往设置页授权',
         })
       }
-    }
-
-    update()
-  },
+    })
+  }
 })
