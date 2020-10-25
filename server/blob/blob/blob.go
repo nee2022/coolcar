44da72007a9383882ebb99a7a66423d4533566af
ccmouse <ccmouse@gmail.com>
2020-10-25 17:50:08 +0800

    implement basic blob service db

--- server/blob/blob/blob.go
+++ server/blob/blob/blob.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	blobpb "coolcar/blob/api/gen/v1"
 	"coolcar/blob/dao"
+	"coolcar/shared/id"
 
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -18,15 +20,40 @@ type Service struct {
 
 // CreateBlob creates a blob.
 func (s *Service) CreateBlob(c context.Context, req *blobpb.CreateBlobRequest) (*blobpb.CreateBlobResponse, error) {
+	aid := id.AccountID(req.AccountId)
+	br, err := s.Mongo.CreateBlob(c, aid)
+	if err != nil {
+		s.Logger.Error("cannot create blob", zap.Error(err))
+		return nil, status.Error(codes.Internal, "")
+	}
 	return nil, status.Error(codes.Unimplemented, "")
 }
 
 // GetBlob gets a blob's contents.
 func (s *Service) GetBlob(c context.Context, req *blobpb.GetBlobRequest) (*blobpb.GetBlobResponse, error) {
+	br, err := s.getBlobRecord(c, id.BlobID(req.Id))
+	if err != nil {
+		return nil, err
+	}
 	return nil, status.Error(codes.Unimplemented, "")
 }
 
 // GetBlobURL gets blob's URL for downloading.
 func (s *Service) GetBlobURL(c context.Context, req *blobpb.GetBlobURLRequest) (*blobpb.GetBlobURLResponse, error) {
+	br, err := s.getBlobRecord(c, id.BlobID(req.Id))
+	if err != nil {
+		return nil, err
+	}
 	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (s *Service) getBlobRecord(c context.Context, bid id.BlobID) (*dao.BlobRecord, error) {
+	br, err := s.Mongo.GetBlob(c, bid)
+	if err == mongo.ErrNoDocuments {
+		return nil, status.Error(codes.NotFound, "")
+	}
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	return br, nil
 }
