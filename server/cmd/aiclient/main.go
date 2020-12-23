162cca199a1e307767a165b21b2fb19db1eacb6f
ccmouse <ccmouse@gmail.com>
2020-12-23 16:35:44 +0800

    pos simulation service play

--- server/cmd/aiclient/main.go
+++ server/cmd/aiclient/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"coolcar/car/mq/amqpclt"
 	coolenvpb "coolcar/shared/coolenv"
+	"coolcar/shared/server"
 	"fmt"
+	"time"
 
+	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 )
 
@@ -16,6 +20,8 @@ func main() {
 
 	ac := coolenvpb.NewAIServiceClient(conn)
 	c := context.Background()
+
+	// Measure distance.
 	res, err := ac.MeasureDistance(c, &coolenvpb.MeasureDistanceRequest{
 		From: &coolenvpb.Location{
 			Latitude:  29.756825521115363,
@@ -31,6 +37,7 @@ func main() {
 	}
 	fmt.Printf("%+v\n", res)
 
+	// Licsense recognition.
 	idRes, err := ac.LicIdentity(c, &coolenvpb.IdentityRequest{
 		Photo: []byte{1, 2, 3, 4, 5},
 	})
@@ -38,4 +45,60 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("%+v\n", idRes)
+
+	// Car position simulation.
+	_, err = ac.SimulateCarPos(c, &coolenvpb.SimulateCarPosRequest{
+		CarId: "car123",
+		InitialPos: &coolenvpb.Location{
+			Latitude:  30,
+			Longitude: 120,
+		},
+		Type: coolenvpb.PosType_NINGBO,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	logger, err := server.NewZapLogger()
+	if err != nil {
+		panic(err)
+	}
+
+	amqpConn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+
+	sub, err := amqpclt.NewSubscriber(amqpConn, "pos_sim", logger)
+	if err != nil {
+		panic(err)
+	}
+
+	ch, cleanUp, err := sub.SubscribeRaw(c)
+	defer cleanUp()
+
+	if err != nil {
+		panic(err)
+	}
+
+	tm := time.After(10 * time.Second)
+	for {
+		shouldStop := false
+		select {
+		case msg := <-ch:
+			fmt.Printf("%s\n", msg.Body)
+		case <-tm:
+			shouldStop = true
+		}
+		if shouldStop {
+			break
+		}
+	}
+
+	_, err = ac.EndSimulateCarPos(c, &coolenvpb.EndSimulateCarPosRequest{
+		CarId: "car123",
+	})
+	if err != nil {
+		panic(err)
+	}
 }
